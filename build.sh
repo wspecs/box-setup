f8f72e182442f4826a9d4f20c19edf3d8969e799
Wendly Saintil <wendly@wendly-macbookpro2.roam.corp.google.com>
2020-03-12 16:58:22 -0700

    add option to setup and override setup

--- build.sh
+++ build.sh
@@ -2,24 +2,32 @@
 
 echo Setting up new box
 
-WSPECS_CONFIG_FILE=/etc/wspecs.build.conf
+WSPECS_CONFIG_FILE=/etc/wspecs/build.conf
+CURRENT_FOLDER=$(pwd)
 
-if [ ! -f "$WSPECS_CONFIG_FILE" ]; then
+if [ ! -f "$WSPECS_CONFIG_FILE"  ] || [[ "$@" == *"--force"* ]]; then
   echo creating config file...
   cat >$WSPECS_CONFIG_FILE<<EOL
 # This configuration file describes all the wspecs package to
 # add to the server. Each block is called in the specific order
 # They are given
-box-functions=HEAD
-box-base=HEAD
+box-functions=v0.0.1
 EOL
 fi
 
-for module in $(cat $WSPECS_CONFIG_FILE | grep =)
+
+for module_info in $(cat $WSPECS_CONFIG_FILE | grep =)
 do
-  IFS=" "
-	read -a module_info <<< "$module"
-	echo "Module name: $module_info[0] "
-	echo "Module version: $module_info[1] "
-  IFS=" "  # Restore the IFS value
+  module=$(echo $module_info | sed 's/=.*//')
+  version=$(echo $module_info | sed 's/.*=//')
+
+  echo checking out wspecs/$module @tag $version
+  build_folder_name=$HOME/$module
+	rm -rf $build_folder_name
+	git clone https://github.com/wspecs/$module.git $build_folder_name
+	cd $build_folder_name
+	git checkout tags/$version -b tmp
+	cat build.sh | sudo -E bash
+	rm -rf $build_folder_name
+	cd $CURRENT_FOLDER
 done
